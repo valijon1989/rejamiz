d395d8548d7ad54f268db2e9681c60ccaa433c4b
valijon1989 <valijon.jon89@gmail.com>
2025-06-11 14:37:32 +0900

    B: feat:CRUP operatorlar

--- app.js
+++ app.js
@@ -38,12 +38,32 @@ app.set("view engine","ejs");
 
 
 // 4.Routing Codes==> ruterlarga muljallangan
-app.post("/create item", (req, res) => {
-    console.log(req);
-    res.json({ test: "success" });
+app.post("/create-item", (req, res) => {
+  console.log("User entered /create-item");
+    console.log(req.body);
+   const new_reja = req.body.reja;
+   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("seccessfully added");
+    }
+   });
 });
+
 app.get("/", function (req, res) {
-    res.render("reja");
+  console.log("User entered /");
+  db.collection("plans")
+  .find()
+  .toArray((err, data) =>{
+    if (err) {
+    console.log(err);
+    res.end("something went wrong");
+  } else {
+    res.render("reja", {items: data});
+     } 
+   });
 });
 
 app.get('/author', (req, res) => {
