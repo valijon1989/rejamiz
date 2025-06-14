b014721b760027730e8a4dcf87ce60e7d673e0e5
valijon1989 <valijon.jon89@gmail.com>
2025-06-15 02:11:03 +0900

    B: feat: D Task bajarib test qilindi

--- train.js
+++ train.js
@@ -210,81 +210,81 @@
 //     resolve(list5[5]);
 //     }, 5000);
    
-//    });
-//  }
-// }
+// //    });
+// //  }
+// // }
 
 
-// async function  run() {
-// let javob = await  maslahatAwait(20);
-// console.log(javob);
-// javob = await  maslahatAwait(33); 
-// console.log(javob); 
-// javob = await  maslahatAwait(43); 
-// console.log(javob); 
-// javob = await  maslahatAwait(62); 
-// console.log(javob); 
-// }
+// // async function  run() {
+// // let javob = await  maslahatAwait(20);
+// // console.log(javob);
+// // javob = await  maslahatAwait(33); 
+// // console.log(javob); 
+// // javob = await  maslahatAwait(43); 
+// // console.log(javob); 
+// // javob = await  maslahatAwait(62); 
+// // console.log(javob); 
+// // }
 
-// run();
-// // qulayligi
-// // Asynchronos ===> ketma- ketlik uchun juda qulay
-// // Promise ===> Promise All arraylarniung mapi bilan juda yaxwi ishlaydi
-// // callback ===> javoblar takrida qulay 
-
-
+// // run();
+// // // qulayligi
+// // // Asynchronos ===> ketma- ketlik uchun juda qulay
+// // // Promise ===> Promise All arraylarniung mapi bilan juda yaxwi ishlaydi
+// // // callback ===> javoblar takrida qulay 
 
 
 
-// console.log("===== PRACTICE =====");
-
-// // DEFINE
-//function qoldiqIbolish(a, b, callback) {
-//     if (b === 0) {
-//         callback("0 ga bo‘lish mumkin emas", null);
-//     } else {
-//         callback(null, a % b);
-//     }
 
 
-// qoldiqIbolish(15, 5, (err, data) => {
-//     if (err) console.log("Error:", err);
-//     else console.log("javob: ", data);
-// });
+// // console.log("===== PRACTICE =====");
+
+// // // DEFINE
+// //function qoldiqIbolish(a, b, callback) {
+// //     if (b === 0) {
+// //         callback("0 ga bo‘lish mumkin emas", null);
+// //     } else {
+// //         callback(null, a % b);
+// //     }
 
 
-// // A task
+// // qoldiqIbolish(15, 5, (err, data) => {
+// //     if (err) console.log("Error:", err);
+// //     else console.log("javob: ", data);
+// // });
 
-// //Harf sifatida kiritilgan birinchi parametr, 
 
-// // kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
-// // Funktsiya tuzing
+// // // A task
 
-// // Masalan: countLetter("e", "engineer")
-// // 'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
-// // 3 sonini qaytaradi
+// // //Harf sifatida kiritilgan birinchi parametr, 
 
-// yasash
-function countLetter(harf, soz) {
-  const result = soz.split(""); // bu yerda split So'zni harflarga bo'lib jamlaydi
-  const lastResult = result.filter((i) => i === harf); // har bir harfni tekshiradi va manaa jura diydi
-  console.log(`
-    ${soz} ning ichida ${harf} dan ${lastResult.length} ta marta bor` ); // bu yerda kerakli matnnini kiritib natijani qanday xolatda olishni suraymiz
+// // // kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
+// // // Funktsiya tuzing
+
+// // // Masalan: countLetter("e", "engineer")
+// // // 'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
+// // // 3 sonini qaytaradi
+
+// // yasash
+// function countLetter(harf, soz) {
+//   const result = soz.split(""); // bu yerda split So'zni harflarga bo'lib jamlaydi
+//   const lastResult = result.filter((i) => i === harf); // har bir harfni tekshiradi va manaa jura diydi
+//   console.log(`
+//     ${soz} ning ichida ${harf} dan ${lastResult.length} ta marta bor` ); // bu yerda kerakli matnnini kiritib natijani qanday xolatda olishni suraymiz
  
-}
+// }
 
-// Call
-countLetter("l", "Alhamdulillah");
+// // Call
+// countLetter("l", "Alhamdulillah");
 
-console.log("############################");
+// console.log("############################");
 
-function countLetter(raqam, parol) {
-    const result1 = parol.split("");
-    const lastResult = result1.filter((i) => i === raqam);
-    console.log(`${parol}ning ichida ${raqam} lardan ${lastResult.length} marta takrorlanib kelgan`);
-}
+// function countLetter(raqam, parol) {
+//     const result1 = parol.split("");
+//     const lastResult = result1.filter((i) => i === raqam);
+//     console.log(`${parol}ning ichida ${raqam} lardan ${lastResult.length} marta takrorlanib kelgan`);
+// }
 
-countLetter("2", "uz24676uz2358uz2456");
+// countLetter("2", "uz24676uz2358uz2456");
 
 
 
@@ -295,8 +295,26 @@ countLetter("2", "uz24676uz2358uz2456");
 
  
 
-// B TASK
- //1- varyant  
+// // B TASK
+//  //1- varyant  
+
+// //   function countDigits(text){
+// //   console.log(typeof text);
+// //   const result = text.split("");
+// //   console.log(result);
+  
+// //   const lastResult = result.filter((ele) => {
+// //     return ele >= 0  && ele !== " ";
+// //   })
+// //   console.log(lastResult);
+// // }
+// //  countDigits("ad2a54f0y79w") ;
+
+
+
+
+// // 2chi varyant
+
 
 //   function countDigits(text){
 //   console.log(typeof text);
@@ -304,29 +322,11 @@ countLetter("2", "uz24676uz2358uz2456");
 //   console.log(result);
   
 //   const lastResult = result.filter((ele) => {
-//     return ele >= 0  && ele !== " ";
+//     return ele >= 7  && ele <= 7;
 //   })
 //   console.log(lastResult);
 // }
 //  countDigits("ad2a54f0y79w") ;
-
-
-
-
-// 2chi varyant
-
-
-  function countDigits(text){
-  console.log(typeof text);
-  const result = text.split("");
-  console.log(result);
-  
-  const lastResult = result.filter((ele) => {
-    return ele >= 7  && ele <= 7;
-  })
-  console.log(lastResult);
-}
- countDigits("ad2a54f0y79w") ;
 
 
 // C TASK
@@ -344,72 +344,91 @@ Hamda classning quyidagdek 3'ta metodi bo'lsin:
 
 Har bir metod ishga tushgan vaqtda log qilinsin
 
-MASALAN:
-const shop = new Shop(4, 5, 2)
+// MASALAN:
+// const shop = new Shop(4, 5, 2)
 
-shop.qoldiq();
-natija qaytishi kerak: Hozir 20: 40'da 4'ta non, 5'ta lag'mon va 2'ta cola mavjud
+// shop.qoldiq();
+// natija qaytishi kerak: Hozir 20: 40'da 4'ta non, 5'ta lag'mon va 2'ta cola mavjud
 
-shop.sotish("non", 3); & shop.qabul("cola", 4); & shop.qoldiq();
-Natija qaytishi kerak: Hozir 20:50da 1ta non, 5ta lag'mon va 6ta cola mavjud!
+// shop.sotish("non", 3); & shop.qabul("cola", 4); & shop.qoldiq();
+// Natija qaytishi kerak: Hozir 20:50da 1ta non, 5ta lag'mon va 6ta cola mavjud!
+// */
+
+// class Shop {
+//   constructor(shopNon, shopLagmon, shopCola) {
+//     this.non = shopNon;
+//     this.lagmon = shopLagmon;
+//     this.cola = shopCola;
+//   }
+// //Shop nomli class tuzdik, va bu class 3 xill parametr qabul qildi
+
+
+//   introduce1() {
+//     console.log(`Sotuvdan oldin: Hozir 20:40 - ${this.non} ta non, ${this.lagmon} ta lagmon, ${this.cola} ta cola mavjud`);
+//   } // malumot bermoqda
+// sotish(nomi, soni) {
+//     if (this[nomi] === undefined) { // agar maxsulotni topa olmasa
+//       console.log(`Error: "${nomi}" nomli mahsulot mavjud emas.`);
+//     } else if (this[nomi] < soni) {  // nomini berilish qiymatiga tenglahanmiz va soni deb sotiluvchi maxsulotning miqdorini belgilaymiz. u boshlangich qiymatdan katta bulganda buni ma'lum qilamiz
+//       console.log(`Error: Yetarli ${nomi} mavjud emas. Hozirda: ${this[nomi]} ta.`);
+//     } else {
+//       this[nomi] -= soni; // agarda ayirilgan xolda yani sotilganda
+//       console.log(`${soni} ta ${nomi} sotildi.`);
+//     }
+//   }
+
+//   qabul(nomi, soni) {
+//     if (this[nomi] === undefined) {
+//       console.log(`Error: "${nomi}" nomli mahsulot mavjud emas.`);
+//     } else {
+//       this[nomi] += soni;
+//       console.log(`${soni} ta ${nomi} qabul qilindi.`);
+//     }
+//   }
+//   introduce() {
+//     console.log(`Qoldiq: Hozir 20:50 - ${this.non} ta non, ${this.lagmon} ta lagmon, ${this.cola} ta cola mavjud`);
+//   }
+// }
+
+// //call qismi
+// const myShop = new Shop(4, 5, 2);
+
+// // Vaqt: 20:40 dagi holat
+// myShop.introduce1("20:40");
+
+// // sotuv
+// myShop.sotish("non", 3);
+
+
+// // qabul
+// myShop.qabul("cola", 7);
+// myShop.qabul("suv", 4);
+// myShop.qabul("lagmon", 10);
+
+
+// // 20:50 dagi xolat
+// myShop.introduce("20:50");
+
+
+
+
+/*
+TASK D
+
+Ikkita parametra ega function tuzing, va functioning
+berilgan birinchi va ikkinchi parametr qiymatlari o'zaro to'liq
+mos kelsa true qiymat qaytarsin
+
+Masalan: checkContent("mitgroup", "gmtiprou");
+Yuqoridagi misolda birinchi va ikkinchi parametr qiymatli bir xil
+ya'ni bir xil harflar qatnashganligi uchun true qiymat qaytaradi.
 */
 
-class Shop {
-  constructor(shopNon, shopLagmon, shopCola) {
-    this.non = shopNon;
-    this.lagmon = shopLagmon;
-    this.cola = shopCola;
-  }
-//Shop nomli class tuzing, va bu class 3 xill parametr qabul qildi
 
+function checkContent(eshmat,  toshmat) {
+const a = eshmat.split("").sort().join("");
+const b = toshmat.split("").sort().join("");
+return a === b;
+};
 
-  introduce1() {
-    console.log(`Sotuvdan oldin: Hozir 20:40 - ${this.non} ta non, ${this.lagmon} ta lagmon, ${this.cola} ta cola mavjud`);
-  } // malumot bermoqda
-sotish(nomi, soni) {
-    if (this[nomi] === undefined) {
-      console.log(`Error: "${nomi}" nomli mahsulot mavjud emas.`);
-    } else if (this[nomi] < soni) {
-      console.log(`Error: Yetarli ${nomi} mavjud emas. Hozirda: ${this[nomi]} ta.`);
-    } else {
-      this[nomi] -= soni;
-      console.log(`${soni} ta ${nomi} sotildi.`);
-    }
-  }
-
-  qabul(nomi, soni) {
-    if (this[nomi] === undefined) {
-      console.log(`Error: "${nomi}" nomli mahsulot mavjud emas.`);
-    } else {
-      this[nomi] += soni;
-      console.log(`${soni} ta ${nomi} qabul qilindi.`);
-    }
-  }
-  introduce() {
-    console.log(`Qoldiq: Hozir 20:50 - ${this.non} ta non, ${this.lagmon} ta lagmon, ${this.cola} ta cola mavjud`);
-  }
-}
-
-//call qismi
-const myShop = new Shop(4, 5, 2);
-
-// Vaqt: 20:40 dagi holat
-myShop.introduce1("20:40");
-
-// sotuv
-myShop.sotish("non", 3);
-
-
-// qabul
-myShop.qabul("cola", 7);
-myShop.qabul("suv", 4);
-myShop.qabul("lagmon", 10);
-
-
-// 20:50 dagi xolat
-myShop.introduce("20:50");
-
-
-
-
-
+console.log(checkContent("mitgroup", "gmtiprou"));
